efc18bf011aa09cf36f241a7e8f9395ee38998ec
Antonio <antonioforte1995@gmail.com>
2020-09-22 12:05:31 +0200

    modified create_dashboards file

--- VIS3_app/create_dashboards.py
+++ VIS3_app/create_dashboards.py
@@ -8,15 +8,19 @@ def create_dashboards(index):
     #-------------------------------
     #VULNERABILITY SUMMARY DASHBOARD
     #-------------------------------
-    os.system('curl -k -XGET \'http://elastic:changeme@3.225.242.97:5601/api/kibana/dashboards/export?dashboard=4500b700-f341-11ea-950f-fba5732a37f6\' -u elastic:changeme 1> vsd_{0}.json'.format(index))
+    x = index.split("_")
+    index_number = x[1]
 
-    with open('vsd_{0}.json'.format(index)) as json_file:
+    
+    os.system('curl -k -XGET \'http://elastic:changeme@3.225.242.97:5601/api/kibana/dashboards/export?dashboard=4500b700-f341-11ea-950f-fba5732a37f6\' -u elastic:changeme 1> vsd.json')
+
+    with open('vsd.json') as json_file:
         data = json.load(json_file)
 
         randomic_id = uuid.uuid1()
 
         data['objects'][0]['id'] = str(randomic_id)
-        data['objects'][0]['attributes']['title'] ="VULNERABILITY SUMMARY DASHBOARD_{0}".format(index)
+        data['objects'][0]['attributes']['title'] ="VULNERABILITY SUMMARY DASHBOARD_{0}".format(index_number)
 
         for i in range(2,5):
             data['objects'][i]['references'][0]['id']="{0}".format(index)
@@ -25,21 +29,21 @@ def create_dashboards(index):
         data['objects'][5]['id']="{0}".format(index)
 
 
-    with open('vsd_{0}.json'.format(index), 'w') as outfile:
+    with open('vsd.json', 'w') as outfile:
         json.dump(data, outfile)
 
     #---------------------------------------------
     #VULNERABILITY TECHNICAL DESCRIPTION DASHBOARD
 
     #---------------------------------------------
-    os.system('curl -k -XGET \'http://elastic:changeme@3.225.242.97:5601/api/kibana/dashboards/export?dashboard=c4cf3880-f341-11ea-950f-fba5732a37f6\' -u elastic:changeme 1> vtd_{0}.json'.format(index))
+    os.system('curl -k -XGET \'http://elastic:changeme@3.225.242.97:5601/api/kibana/dashboards/export?dashboard=c4cf3880-f341-11ea-950f-fba5732a37f6\' -u elastic:changeme 1> vtd.json')
 
-    with open('vtd_{0}.json'.format(index)) as json_file:
+    with open('vtd.json') as json_file:
         data = json.load(json_file)
         randomic_id = uuid.uuid1()
 
         data['objects'][0]['id'] = str(randomic_id)
-        data['objects'][0]['attributes']['title'] ="VULNERABILITY TECHNICAL DESCRIPTION DASHBOARD_{0}".format(index)
+        data['objects'][0]['attributes']['title'] ="VULNERABILITY TECHNICAL DESCRIPTION DASHBOARD_{0}".format(index_number)
         
         for i in range(2,4):
             data['objects'][i]['references'][0]['id']="{0}".format(index)
@@ -47,22 +51,22 @@ def create_dashboards(index):
         data['objects'][4]['id']="{0}".format(index)
         data['objects'][4]['attributes']['title']="{0}".format(index)
 
-    with open('vtd_{0}.json'.format(index), 'w') as outfile:
+    with open('vtd.json', 'w') as outfile:
         json.dump(data, outfile)
         
 
     #----------------------
     #EXPLOIT VIEW DASHBOARD
     #----------------------
-    os.system('curl -k -XGET \'http://elastic:changeme@3.225.242.97:5601/api/kibana/dashboards/export?dashboard=ffb2dc74-fcb0-11ea-ba23-000c29533237\' -u elastic:changeme 1> ev_{0}.json'.format(index))
+    os.system('curl -k -XGET \'http://elastic:changeme@3.225.242.97:5601/api/kibana/dashboards/export?dashboard=ffb2dc74-fcb0-11ea-ba23-000c29533237\' -u elastic:changeme 1> ev.json')
 
-    with open('ev_{0}.json'.format(index)) as json_file:
+    with open('ev.json') as json_file:
         data = json.load(json_file)
         randomic_id = uuid.uuid1()
 
         data['objects'][0]['id'] = str(randomic_id)
 
-        data['objects'][0]['attributes']['title'] ="EXPLOIT VIEW DASHBOARD_{0}".format(index)
+        data['objects'][0]['attributes']['title'] ="EXPLOIT VIEW DASHBOARD_{0}".format(index_number)
         
         for i in range(2,4):
             data['objects'][i]['references'][0]['id']="{0}".format(index)
@@ -71,10 +75,11 @@ def create_dashboards(index):
         data['objects'][4]['id']="{0}".format(index)
 
 
-    with open('ev_{0}.json'.format(index), 'w') as outfile:
+    with open('ev.json', 'w') as outfile:
         json.dump(data, outfile)
 
         
-    os.system('curl -u elastic:changeme -k -XPOST \'http://elastic:changeme@3.225.242.97:5601/api/kibana/dashboards/import\' -H \'Content-Type: application/json\' -H "kbn-xsrf: true" -d @vsd_{0}.json'.format(index))
-    os.system('curl -u elastic:changeme -k -XPOST \'http://elastic:changeme@3.225.242.97:5601/api/kibana/dashboards/import\' -H \'Content-Type: application/json\' -H "kbn-xsrf: true" -d @vtd_{0}.json'.format(index))
-    os.system('curl -u elastic:changeme -k -XPOST \'http://elastic:changeme@3.225.242.97:5601/api/kibana/dashboards/import\' -H \'Content-Type: application/json\' -H "kbn-xsrf: true" -d @ev_{0}.json'.format(index))
+    os.system('curl -u elastic:changeme -k -XPOST \'http://elastic:changeme@3.225.242.97:5601/api/kibana/dashboards/import\' -H \'Content-Type: application/json\' -H "kbn-xsrf: true" -d @vsd.json')
+    os.system('curl -u elastic:changeme -k -XPOST \'http://elastic:changeme@3.225.242.97:5601/api/kibana/dashboards/import\' -H \'Content-Type: application/json\' -H "kbn-xsrf: true" -d @vtd.json')
+    os.system('curl -u elastic:changeme -k -XPOST \'http://elastic:changeme@3.225.242.97:5601/api/kibana/dashboards/import\' -H \'Content-Type: application/json\' -H "kbn-xsrf: true" -d @ev.json')
+    
